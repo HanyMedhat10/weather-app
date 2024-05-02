764a66aa88da609bc0da0ff582e46b2795409be9
Hany Medhat <hany.medhat24@gmail.com>
2024-05-02 12:38:18 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/Hany/weather_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.Hany.weather_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/views/home_view.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/views/search_view.dart';
+import 'package:weather_app/widgets/no_weather_body.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Weather App',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return const SearchView();
+                    },
+                  ),
+                );
+              },
+              icon: const Icon(
+                Icons.search,
+                color: Colors.white,
+              ))
+        ],
+      ),
+      body: const NoWeatherBody(),
+    );
+  }
+}
